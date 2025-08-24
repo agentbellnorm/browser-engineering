7f0e12e8021f2d7e2207fd5084b3328c2c6b6167
Morgan Bentell <morgan.bentell@gmail.com>
2025-08-24 23:35:59 +0200

    fix(url): handle localhost url with port properly

--- src/url.rs
+++ src/url.rs
@@ -1,12 +1,54 @@
 use std::error::Error;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct URL {
     pub scheme: String,
     pub host: String,
     pub port: Option<String>,
     pub path: String,
     pub query: Option<String>,
+}
+
+#[test]
+fn parse_localhost() {
+    assert_eq!(
+        URL::parse("http://localhost:3000/lol.html").expect("hej"),
+        URL {
+            scheme: "http".to_string(),
+            host: "localhost".to_string(),
+            port: Some("3000".to_string()),
+            path: "/lol.html".to_string(),
+            query: None,
+        }
+    )
+}
+
+#[test]
+fn parse_http() {
+    assert_eq!(
+        URL::parse("http://example.com/hej").expect("hej"),
+        URL {
+            scheme: "http".to_string(),
+            host: "example.com".to_string(),
+            port: None,
+            path: "/hej".to_string(),
+            query: None,
+        }
+    )
+}
+
+#[test]
+fn parse_https() {
+    assert_eq!(
+        URL::parse("https://example.com/hej").expect("hej"),
+        URL {
+            scheme: "https".to_string(),
+            host: "example.com".to_string(),
+            port: None,
+            path: "/hej".to_string(),
+            query: None,
+        }
+    )
 }
 
 impl URL {
@@ -20,15 +62,23 @@ impl URL {
             let mut parts2 = r1_fixed.splitn(2, "/");
             let (host, rest2) = (parts2.next(), parts2.next());
 
-            if let (Some(h), Some(r2)) = (host, rest2) {
+            if let (Some(mut h), Some(r2)) = (host, rest2) {
                 let path = &format!("/{r2}");
+                let mut port: Option<String> = None;
+
+                if h.contains(":") {
+                    let mut parts3 = h.splitn(2, ":");
+                    let (host_without_port, parsed_port) = (parts3.next(), parts3.next());
+                    port = parsed_port.map(|slice| slice.to_string());
+                    h = host_without_port.ok_or("no host")?;
+                }
 
                 return Ok(URL {
                     scheme: s.to_string(),
                     host: h.to_string(),
                     path: path.to_string(),
                     query: None,
-                    port: None,
+                    port,
                 });
             }
         }
